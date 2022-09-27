f2bde3988c3b53745a83c5002a17652264a5c9b5
Mattiew Boutin <1742305@cegepba.qc.ca>
2022-09-27 13:41:30 -0400

    création de CaseSerpent et code!

---
+++ Travail1/Models/Case/CaseSerpent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travail1.Models.Point;
+
+namespace Travail1.Models.Case
+{
+    internal class CaseSerpent : Case
+    {
+        public CaseSerpent(Points points, int position) : base(points, position)
+        {
+            this.position = position;
+        }
+        public void genererCaseSerpent()
+        {
+            int positionInitial = position;
+            int positionFinal = position - 10;// modifier le 10 par la variable qui va indiquer la fin du serpent
+            position = position - (positionFinal - positionInitial);
+
+        }
+    }
+}
