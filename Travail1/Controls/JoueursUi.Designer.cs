d6d3a8ffa4bb689d8753e74dc6cc1889ba2ee95e
desmont323 <tom.fortin@hotmail.ca>
2022-09-27 13:51:45 -0400

    affichage joueur ui design

---
+++ Travail1/Controls/JoueursUi.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Travail1.Controls
+{
+    partial class JoueursUi
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTextJoueur1 = new System.Windows.Forms.Label();
+            this.lblTextJoueur2 = new System.Windows.Forms.Label();
+            this.lblNomJoueur1 = new System.Windows.Forms.Label();
+            this.lblNomJoueur2 = new System.Windows.Forms.Label();
+            this.lblPointJoueur1 = new System.Windows.Forms.Label();
+            this.lblTextPointJoueur1 = new System.Windows.Forms.Label();
+            this.lblTextPointJoueur2 = new System.Windows.Forms.Label();
+            this.lblPointJoueur2 = new System.Windows.Forms.Label();
+            this.lblTextTourJoueur = new System.Windows.Forms.Label();
+            this.lblJoueur = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTextJoueur1
+            // 
+            this.lblTextJoueur1.AutoSize = true;
+            this.lblTextJoueur1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTextJoueur1.Location = new System.Drawing.Point(3, 109);
+            this.lblTextJoueur1.Name = "lblTextJoueur1";
+            this.lblTextJoueur1.Size = new System.Drawing.Size(118, 32);
+            this.lblTextJoueur1.TabIndex = 0;
+            this.lblTextJoueur1.Text = "Joueur 1 :";
+            // 
+            // lblTextJoueur2
+            // 
+            this.lblTextJoueur2.AutoSize = true;
+            this.lblTextJoueur2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTextJoueur2.Location = new System.Drawing.Point(3, 224);
+            this.lblTextJoueur2.Name = "lblTextJoueur2";
+            this.lblTextJoueur2.Size = new System.Drawing.Size(118, 32);
+            this.lblTextJoueur2.TabIndex = 1;
+            this.lblTextJoueur2.Text = "Joueur 2 :";
+            // 
+            // lblNomJoueur1
+            // 
+            this.lblNomJoueur1.AutoSize = true;
+            this.lblNomJoueur1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNomJoueur1.Location = new System.Drawing.Point(185, 109);
+            this.lblNomJoueur1.Name = "lblNomJoueur1";
+            this.lblNomJoueur1.Size = new System.Drawing.Size(106, 32);
+            this.lblNomJoueur1.TabIndex = 2;
+            this.lblNomJoueur1.Text = "Joueur 1";
+            // 
+            // lblNomJoueur2
+            // 
+            this.lblNomJoueur2.AutoSize = true;
+            this.lblNomJoueur2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNomJoueur2.Location = new System.Drawing.Point(127, 224);
+            this.lblNomJoueur2.Name = "lblNomJoueur2";
+            this.lblNomJoueur2.Size = new System.Drawing.Size(106, 32);
+            this.lblNomJoueur2.TabIndex = 3;
+            this.lblNomJoueur2.Text = "Joueur 2";
+            // 
+            // lblPointJoueur1
+            // 
+            this.lblPointJoueur1.AutoSize = true;
+            this.lblPointJoueur1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPointJoueur1.Location = new System.Drawing.Point(185, 141);
+            this.lblPointJoueur1.Name = "lblPointJoueur1";
+            this.lblPointJoueur1.Size = new System.Drawing.Size(164, 32);
+            this.lblPointJoueur1.TabIndex = 4;
+            this.lblPointJoueur1.Text = "Point joueur 1";
+            // 
+            // lblTextPointJoueur1
+            // 
+            this.lblTextPointJoueur1.AutoSize = true;
+            this.lblTextPointJoueur1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTextPointJoueur1.Location = new System.Drawing.Point(3, 141);
+            this.lblTextPointJoueur1.Name = "lblTextPointJoueur1";
+            this.lblTextPointJoueur1.Size = new System.Drawing.Size(176, 32);
+            this.lblTextPointJoueur1.TabIndex = 5;
+            this.lblTextPointJoueur1.Text = "Point joueur 1 :";
+            // 
+            // lblTextPointJoueur2
+            // 
+            this.lblTextPointJoueur2.AutoSize = true;
+            this.lblTextPointJoueur2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTextPointJoueur2.Location = new System.Drawing.Point(3, 256);
+            this.lblTextPointJoueur2.Name = "lblTextPointJoueur2";
+            this.lblTextPointJoueur2.Size = new System.Drawing.Size(176, 32);
+            this.lblTextPointJoueur2.TabIndex = 7;
+            this.lblTextPointJoueur2.Text = "Point joueur 2 :";
+            // 
+            // lblPointJoueur2
+            // 
+            this.lblPointJoueur2.AutoSize = true;
+            this.lblPointJoueur2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPointJoueur2.Location = new System.Drawing.Point(185, 256);
+            this.lblPointJoueur2.Name = "lblPointJoueur2";
+            this.lblPointJoueur2.Size = new System.Drawing.Size(164, 32);
+            this.lblPointJoueur2.TabIndex = 6;
+            this.lblPointJoueur2.Text = "Point joueur 1";
+            // 
+            // lblTextTourJoueur
+            // 
+            this.lblTextTourJoueur.AutoSize = true;
+            this.lblTextTourJoueur.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTextTourJoueur.Location = new System.Drawing.Point(3, 28);
+            this.lblTextTourJoueur.Name = "lblTextTourJoueur";
+            this.lblTextTourJoueur.Size = new System.Drawing.Size(219, 40);
+            this.lblTextTourJoueur.TabIndex = 8;
+            this.lblTextTourJoueur.Text = "Tour du Joueur :";
+            // 
+            // lblJoueur
+            // 
+            this.lblJoueur.AutoSize = true;
+            this.lblJoueur.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblJoueur.Location = new System.Drawing.Point(228, 28);
+            this.lblJoueur.Name = "lblJoueur";
+            this.lblJoueur.Size = new System.Drawing.Size(101, 40);
+            this.lblJoueur.TabIndex = 9;
+            this.lblJoueur.Text = "Joueur";
+            // 
+            // JoueursUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.lblJoueur);
+            this.Controls.Add(this.lblTextTourJoueur);
+            this.Controls.Add(this.lblTextPointJoueur2);
+            this.Controls.Add(this.lblPointJoueur2);
+            this.Controls.Add(this.lblTextPointJoueur1);
+            this.Controls.Add(this.lblPointJoueur1);
+            this.Controls.Add(this.lblNomJoueur2);
+            this.Controls.Add(this.lblNomJoueur1);
+            this.Controls.Add(this.lblTextJoueur2);
+            this.Controls.Add(this.lblTextJoueur1);
+            this.Name = "JoueursUi";
+            this.Size = new System.Drawing.Size(359, 304);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblTextJoueur1;
+        private Label lblTextJoueur2;
+        private Label lblNomJoueur1;
+        private Label lblNomJoueur2;
+        private Label lblPointJoueur1;
+        private Label lblTextPointJoueur1;
+        private Label lblTextPointJoueur2;
+        private Label lblPointJoueur2;
+        private Label lblTextTourJoueur;
+        private Label lblJoueur;
+    }
+}
---
+++ Travail1/Controls/JoueursUi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Travail1.Controls
+{
+    public partial class JoueursUi : UserControl
+    {
+        public JoueursUi()
+        {
+            InitializeComponent();
+        }
+    }
+}
