7daec104763fbd5e101bc8f37f1ec1a0f32c3e76
1742305 <mattiew76@live.ca>
2022-09-28 08:50:27 -0400

    ajout case trappe

---
+++ Travail1/Models/Case/CaseTrappe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travail1.Models.Point;
+
+namespace Travail1.Models.Case
+{
+    internal class CaseTrappe : Case
+    {
+        public CaseTrappe(Points points, int position) : base(points, position)
+        {
+            this.position = position;
+        }
+        public void diminunerPosition()
+        {
+            Random rand = new Random();
+            int nbreCaseADiminuer;
+            nbreCaseADiminuer = rand.Next(5, 11);
+            position = position - nbreCaseADiminuer;
+        }
+        public override string ToString()
+        {
+            return position.ToString();
+        }
+    }
+}
