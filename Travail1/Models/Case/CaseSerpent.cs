a43b3e5ebdc277dffe2f69ecf42222980b6fd928
1742305 <mattiew76@live.ca>
2022-09-28 08:34:40 -0400

    ajout override position

--- Travail1/Models/Case/CaseSerpent.cs
+++ Travail1/Models/Case/CaseSerpent.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Travail1.Models.Point;
 
 namespace Travail1.Models.Case
 {
-    internal class CaseSerpent
+    internal class CaseSerpent : Case
     {
+        public CaseSerpent(Points points, int position) : base(points, position)
+        {
+            this.position = position;
+        }
+        public void genererCaseSerpent()
+        {
+            int positionInitial = position;
+            int positionFinal = position - 10;// modifier le 10 par la variable qui va indiquer la fin du serpent
+            position = position + (positionInitial - positionFinal);
 
+        }
+        public override string ToString()
+        {
+            return position.ToString();
+        }
     }
 }
