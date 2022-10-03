2693facad0bfec1d5bb00fe834bd16524d7423bd
Mattiew Boutin <mattiew76@LIVE.CA>
2022-10-03 14:21:42 -0400

    case trappe dessin

--- Travail1/Models/Case/CaseTrappe.cs
+++ Travail1/Models/Case/CaseTrappe.cs
@@ -9,9 +9,39 @@ namespace Travail1.Models.Case
 {
     internal class CaseTrappe : Case
     {
-        public CaseTrappe(Points points, int position) : base(points, position)
+        protected Points points;
+        protected int position;
+        private int largeur;
+        public int Points { get => points.ObtenirPoints(); }
+        public CaseSerpent(Points points, int position) : base(points, position)
         {
+            this.points = points;
             this.position = position;
+            this.largeur = 100;
+        }
+        public int trappePosition()
+        {
+
+            De de3 = new De(3);
+            position = position + de3.brasser();
+            return position;
+        }
+        protected PointF ObtenirCoordonees()
+        {
+            int y = (7 - (position / 8));
+            int x = (position % 8);
+            if ((position / 8) % 2 == 1)
+            {
+                x = 7 - x;
+            }
+            return new PointF { X = x * largeur, Y = y * largeur };
+        }
+        public override void Dessiner(Graphics graphics)
+        {
+            var coordonees = ObtenirCoordonees();
+            var font = new Font("Calibri", 20);
+            graphics.DrawRectangle(Pens.green, coordonees.X, coordonees.Y, largeur, largeur);
+            graphics.DrawString((position + 1).ToString(), font, Brushes.Blue, coordonees.X + 30, coordonees.Y + 30);
         }
         public void diminunerPosition()
         {
@@ -19,10 +49,6 @@ namespace Travail1.Models.Case
             int nbreCaseADiminuer;
             nbreCaseADiminuer = rand.Next(5, 11);
             position = position - nbreCaseADiminuer;
-        }
-        public override string ToString()
-        {
-            return position.ToString();
         }
     }
 }
