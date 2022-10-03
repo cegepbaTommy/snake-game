3330342cf43a45e8a788b9da2f4e57e9ceef80b5
Mattiew Boutin <mattiew76@LIVE.CA>
2022-10-03 14:18:15 -0400

    case abstract final

---
+++ Travail1/Models/Case/CaseAbstract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travail1.Models.Case
+{
+    class CaseAbstract
+    {
+        protected Points points;
+        protected int position;
+        private int largeur;
+
+        public int Points { get => points.ObtenirPoints(); }
+
+        public CaseAbstract(Points points, int position)
+        {
+            this.points = points;
+            this.position = position;
+            this.largeur = 100;
+        }
+        public abstract void Dessiner(Graphics graphics);
+    }
+}
