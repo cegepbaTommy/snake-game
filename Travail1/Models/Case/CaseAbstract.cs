f75663e2f3b88d73d673849262d9df5fd48907dd
Mattiew Boutin <mattiew76@LIVE.CA>
2022-10-03 14:17:00 -0400

    case abstarct final

--- Travail1/Models/Case/CaseAbstract.cs
+++ Travail1/Models/Case/CaseAbstract.cs
@@ -8,6 +8,18 @@ namespace Travail1.Models.Case
 {
     class CaseAbstract
     {
+        protected Points points;
+        protected int position;
+        private int largeur;
 
+        public int Points { get => points.ObtenirPoints(); }
+
+        public CaseAbstract(Points points, int position)
+        {
+            this.points = points;
+            this.position = position;
+            this.largeur = 100;
+        }
+        public abstract void Dessiner(Graphics graphics);
     }
 }
